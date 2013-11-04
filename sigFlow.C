393f1564959173e02c439298189b63a09773d92f
koskot77 <koskot77@server.fake>
2013-11-04 05:05:39 -0500

    synch

--- sigFlow.C
+++ sigFlow.C
@@ -1,54 +1,59 @@
 #include<iomanip>
 
+int scenario=3;
+
 void sigFlow(void){
 TChain *sig  = new TChain("micro");
-sig->AddFile("/data/users/kkotov/2013.05.12/micro_signals_metSel.root");
-sig->AddFile("/data/users/kkotov/2013.05.12/micro_signalsMore_metSel.root");
+sig->AddFile("micro_signals_metSel.root");
 
 const char *cut[] = {
-"Stage -1: Number generated          ",
-"Stage  0: Loose muon veto           ",
-"Stage  1: Loose electron veto       ",
-"Stage  2: $\\geq$ 1 jet $>$60~\\GeVc ",
-"Stage  3: $\\geq$ 2 jets $>$60~\\GeVc",
-"Stage  4: $\\geq$ 3 jets $>$40~\\GeVc",
+"Stage -1: Total expected             ",
+"Stage  0: Loose electon veto         ",
+"Stage  1: Loose muon veto            ",
+"Stage  2: $\\geq$ 1 jet $>$60~\\GeVc   ",
+"Stage  3: $\\geq$ 2 jets $>$60~\\GeVc  ",
+"Stage  4: $\\geq$ 3 jets $>$40~\\GeVc  ",
 "Stage  5: 4$^{th}$ jet \\pt$<$35~\\GeVc",
-"Stage  6: $M(3 jets)<$250~\\GeVcc",
+"Stage  6: $M(3 jets)<$250~\\GeVcc     ",
 
-"Stage  7: $E_T^{Miss} >$250~\\Ge/c  ",
-"Stage  8:    veto  CSVl $b$-tag      ",
-"Stage  8:        0 CSVm $b$-tag      ",
-"Stage  8:        1 CSVm $b$-tag      ",
-"Stage  8:        2 CSVm $b$-tag      ",
+"Stage  7: $E_T^{Miss} >$250~\\GeV     ",
+"\%Stage  7:    Veto  CSVl  b tags      ",
+"~~~Stage  7: With   0 CSVm b tags    ",
+"~~~Stage  7: With   1 CSVm b tag     ",
+"\%~~~Stage  7: With   2 CSVm b tags    ",
 
-"Stage  8: $E_T^{Miss} >$300~\\GeVc  ",
-"Stage  9:    veto  CSVl $b$-tag      ",
-"Stage  9:        0 CSVm $b$-tag      ",
-"Stage  9:        1 CSVm $b$-tag      ",
-"Stage  9:        2 CSVm $b$-tag      ",
+"Stage  8: $E_T^{Miss} >$300~\\GeVc    ",
+"\%Stage  8:    Veto  CSVl  b tags      ",
+"~~~Stage  8: With   0 CSVm b tags    ",
+"~~~Stage  8: With   1 CSVm b tag     ",
+"\%~~~Stage  8: With   2 CSVm b tags    ",
 
-"Stage  9: $E_T^{Miss} >$350~\\GeVc   ",
-"Stage 10:    veto  CSVl $b$-tag      ",
-"Stage 10:        0 CSVm $b$-tag      ",
-"Stage 10:        1 CSVm $b$-tag      ",
-"Stage 10:        2 CSVm $b$-tag      ",
+"Stage  9: $E_T^{Miss} >$350~\\GeV     ",
+"\%~~~Stage  9:    Veto  CSVl b tags    ",
+"~~~Stage  9: With   0 CSVm b tags    ",
+"~~~Stage  9: With   1 CSVm b tag     ",
+"\%~~~Stage  9: With   2 CSVm b tags    ",
 
-"Stage 10: $E_T^{Miss} >$400~\\GeVc  ",
-"Stage 11:    veto  CSVl $b$-tag      ",
-"Stage 11:        0 CSVm $b$-tag      ",
-"Stage 11:        1 CSVm $b$-tag      ",
-"Stage 11:        2 CSVm $b$-tag      "
+"Stage 10: $E_T^{Miss} >$400~\\GeVc    ",
+"\%~~~Stage 10:    Veto  CSVl b tags    ",
+"~~~Stage 10: With   0 CSVm b tags    ",
+"~~~Stage 10: With   1 CSVm b tag     ",
+"\%~~~Stage 10: With   2 CSVm b tags    "
 };
 
-TH1F *tmp = new TH1F("tmp","",-100,0,10000);
+TH1F *tmp = new TH1F("tmp","",10000,0,10000);
 
-enum {S3m0=47, S3m50=50, S3m100=52, S3m150=53, S3m200=54, S3m300=55, S3m400=67, S3m500=56, S3m600=68,
-      S4m0=27, S4m50=30, S4m100=32, S4m150=33, S4m200=34, S4m300=35, S4m400=69, S4m500=36, S4m600=70, S4m700=71, S4m800=72, S4m900=73, S4m1000=74};
+enum {S3m0=47, S3m50=50, S3m100=52, S3m150=53, S3m200=54, S3m300=55, S3m400=75, S3m500=56, S3m600=76,
+      S4m0=27, S4m50=30, S4m100=32, S4m150=33, S4m200=34, S4m300=35, S4m400=77, S4m500=36, S4m600=78, S4m700=79, S4m800=80, S4m900=81, S4m1000=82};
 
 //double lumiScale = (0.802 + 4.403 + 6.388 + 7.248)/5.;
 double lumiScale = ( 19.6 )/5.;
 
-cout<<"Cut                               &  0  &  50  &  100  &  150  &  200  &  300  &  400  &  500  &  600  \\\\ \\hline "<<endl;
+if( scenario==3 )
+cout<<"Cut                                   &    0    &    50   &   100   &   150   &   200   &   300   &   400   &   500   &   600   \\\\ \\hline "<<endl;
+else
+cout<<"Cut                                   &   150   &   200   &   300   &   400   &   500   &   600   &   700   &   800   &   900   \\\\ \\hline "<<endl;
+
 for(int sel=-1; sel<27; sel++){
   char buff[512];
   switch( sel ){
@@ -94,6 +99,8 @@ for(int sel=-1; sel<27; sel++){
   }
 
   char buff2[1024];
+
+if( scenario==3 ){
   sprintf(buff2,"%s*(type==%d)",buff,S3m0); 
   sig->Draw("type>>tmp",buff2);
   double sig0yield = tmp->Integral() * lumiScale;
@@ -124,6 +131,40 @@ for(int sel=-1; sel<27; sel++){
 
 
   cout<<cut[sel+1]<<" & "<<setw(7)<<int(sig0yield+0.5)<<" & "<<setw(7)<<int(sig50yield+0.5)<<" & "<<setw(7)<<int(sig100yield+0.5)<<" & "<<setw(7)<<int(sig150yield+0.5)<<" & "<<setw(7)<<int(sig200yield+0.5)<<" & "<<setw(7)<<int(sig300yield+0.5)<<" & "<<setw(7)<<int(sig400yield+0.5)<<" & "<<setw(7)<<int(sig500yield+0.5)<<" & "<<setw(7)<<int(sig600yield+0.5)<<" \\\\ "<<endl;
+} else {
+  sprintf(buff2,"%s*(type==%d)",buff,S4m150); 
+  sig->Draw("type>>tmp",buff2);
+  double sig150yield = tmp->Integral() * lumiScale;
+  sprintf(buff2,"%s*(type==%d)",buff,S4m200); 
+  sig->Draw("type>>tmp",buff2);
+  double sig200yield = tmp->Integral() * lumiScale;
+  sprintf(buff2,"%s*(type==%d)",buff,S4m300); 
+  sig->Draw("type>>tmp",buff2);
+  double sig300yield  = tmp->Integral() * lumiScale;
+  sprintf(buff2,"%s*(type==%d)",buff,S4m400);
+  sig->Draw("type>>tmp",buff2);
+  double sig400yield = tmp->Integral() * lumiScale;
+  sprintf(buff2,"%s*(type==%d)",buff,S4m500); 
+  sig->Draw("type>>tmp",buff2);
+  double sig500yield  = tmp->Integral() * lumiScale;
+  sprintf(buff2,"%s*(type==%d)",buff,S4m600); 
+  sig->Draw("type>>tmp",buff2);
+  double sig600yield  = tmp->Integral() * lumiScale;
+  sprintf(buff2,"%s*(type==%d)",buff,S4m700); 
+  sig->Draw("type>>tmp",buff2);
+  double sig700yield  = tmp->Integral() * lumiScale;
+  sprintf(buff2,"%s*(type==%d)",buff,S4m800); 
+  sig->Draw("type>>tmp",buff2);
+  double sig800yield  = tmp->Integral() * lumiScale;
+  sprintf(buff2,"%s*(type==%d)",buff,S4m900); 
+  sig->Draw("type>>tmp",buff2);
+  double sig900yield  = tmp->Integral() * lumiScale;
+
+
+  cout<<cut[sel+1]<<" & "<<setw(7)<<int(sig150yield+0.5)<<" & "<<setw(7)<<int(sig200yield+0.5)<<" & "<<setw(7)<<int(sig300yield+0.5)<<" & "<<setw(7)<<int(sig400yield+0.5)<<" & "<<setw(7)<<int(sig500yield+0.5)<<" & "<<setw(7)<<int(sig600yield+0.5)<<" & "<<setw(7)<<int(sig700yield+0.5)<<" & "<<setw(7)<<int(sig800yield+0.5)<<" & "<<setw(7)<<int(sig900yield+0.5)<<" \\\\ "<<endl;
+
+}
+
 }
 
 }
